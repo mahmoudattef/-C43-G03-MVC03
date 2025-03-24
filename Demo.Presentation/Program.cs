573c41426a07dd2cb96da9859777452c75b07a57
mahmoudattef <mahmoudatef057@gmail.com>
2025-03-24 21:18:25 +0200

    Session 4

--- Demo.Presentation/Program.cs
+++ Demo.Presentation/Program.cs
@@ -15,7 +15,7 @@ namespace Demo.Presentation
 
             #region Add services to the container.
 
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews(); 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
          {  
              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
